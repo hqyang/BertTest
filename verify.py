869a42e1295ddd7e2444a6a74bf549c3ebfff1d8
Haiqin Yang <haiqin.yang@gmail.com>
2019-02-26 15:51:33 +0800

    add test code to compute the number of model parameters in verify.py

--- verify.py
+++ verify.py
@@ -8,3 +8,18 @@ Feature:
 
 Scenario: 
 """
+
+def calSize(H, vs, mpe, L):
+    for l in L:
+        # embedding: (vs+mpe)*H; # Query, Key, value: 3*H*H; Intermediate: 4*H *H; Pooler: H*H
+        sz = (vs+mpe)*H + ((3+4)*H*H)*l + H*H
+        print('# layer: '+str(l)+', #para: '+str(sz))
+
+
+if __name__=='__main__':
+    H = 768
+    vs = 21128
+    mpe = 512
+    L = [3, 6, 12]
+
+    num_model_para = calSize(H, vs, mpe, L)
