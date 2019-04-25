e8f045e2b205e6d867b5cbef2fa8b458864aadf3
Haiqin Yang <haiqin.yang@gmail.com>
2019-04-25 16:41:55 +0800

    add code to call SIGHAN_BAKEOFF

--- Eval_via_SIGHAN_Bakeoff/eval_ontonotes.py
+++ Eval_via_SIGHAN_Bakeoff/eval_ontonotes.py
@@ -55,9 +55,27 @@ def proc_ontonotes_for_prediction(data_dir, output_dir):
         print('Finish processing ' + part + ' file!')
 
 
-SAVE_DATA = True
+def output_sighan_rs(data_dir, pred_dir):
+    parts = ['dev', 'train', 'test']
+
+    dict_file = os.path.join(data_dir, 'ontonotes_train_words.txt')
+
+    for part in parts:
+        true_seg_text = os.path.join(data_dir, 'ontonotes_'+part+'_seg.txt')
+        pred_seg_text = os.path.join(pred_dir, 'ontonotes_'+part+'_seg.txt') # seg data
+        outscore_text = os.path.join(pred_dir, 'ontonotes_'+part+'_score.txt')
+
+        os.system('score ' + dict_file + ' ' + true_seg_text + ' ' + pred_seg_text + ' > ' + outscore_text)
+
+
+
 bLocal = True
 bLocal = False
+
+SAVE_DATA = True
+SAVE_DATA = False
+
+SIGHAN_BAKEOFF = True
 
 if __name__=='__main__':
     if SAVE_DATA:
@@ -66,6 +84,17 @@ if __name__=='__main__':
         else:
             data_dir = '../../data/ontonotes5/4ner_data/'
 
-        output_dir = data_dir + 'eval_data/'
+            output_dir = data_dir + 'eval_data/'
+
         os.makedirs(output_dir, exist_ok=True)
         proc_ontonotes_for_prediction(data_dir, output_dir)
+
+    if SIGHAN_BAKEOFF:
+        if bLocal:
+            data_dir = '/Users/haiqinyang/Downloads/datasets/ontonotes-release-5.0/ontonote_data/proc_data/4ner_data/eval_data'
+            pred_dir = '/Users/haiqinyang/Downloads/datasets/ontonotes-release-5.0/ontonote_data/proc_data/eval/2019_3_22/pred/'
+        else:
+            data_dir = '../../data/ontonotes5/4ner_data/eval_data/'
+            pred_dir = './tmp_2019_3_22/out/ontonotes_eval/'
+
+        output_sighan_bakeoff_rs(data_dir, pred_dir)
