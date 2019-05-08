3fdd8570c7d91daa86d1eabe998ccd7ac92b9be8
Haiqin Yang <haiqin.yang@gmail.com>
2019-05-08 10:26:39 +0800

    set learning_rate to 1e-3

--- BertSoftmaxCWSDataloaderTest.py
+++ BertSoftmaxCWSDataloaderTest.py
@@ -404,7 +404,7 @@ def set_server_eval_4CWS_param():
             'init_checkpoint': '../models/bert-base-chinese/',
             'visible_device': 1,
             'num_train_epochs': 10,
-            'learning_rate': 1e-2,
+            'learning_rate': 1e-3,
             'override_output': True,
             }
 
