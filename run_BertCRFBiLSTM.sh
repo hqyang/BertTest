8953c48460141cfc1f9b92708b6d0f65986da3c5
Haiqin Yang <haiqin.yang@gmail.com>
2019-05-08 18:10:45 +0800

    update run_BertCRFBiLSTM.sh

--- run_BertCRFBiLSTM.sh
+++ run_BertCRFBiLSTM.sh
@@ -6,10 +6,10 @@ python BertCRFCWSDataloaderBiLSTMTest.py \
     --data_dir ../data/CWS/BMES/MSR/ \
     --bert_model_dir ../models/bert-base-chinese/ \
     --vocab_file ../models/bert-base-chinese/vocab.txt \
-    --output_dir ./tmp/4CWS/MSR/CRF_BiLSTM_l12,
+    --output_dir ./tmp/4CWS/MSR/CRF_BiLSTM_l12, \
     --do_lower_case True \
     --max_seq_length 128 \
-    --num_hidden_layers 12
+    --num_hidden_layers 12 \
     --init_checkpoint ../models/bert-base-chinese\
     --train_batch_size 32 \
     --override_output True \
