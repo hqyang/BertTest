dccc138666740edf61fe5c0519216f449c6fc6a2
Haiqin Yang <haiqin.yang@gmail.com>
2019-05-09 17:36:53 +0800

    add 7 script files for testing BertSoftmaxVariant

---
+++ run_BertSoftmaxVariant_AllFinetune.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+python BertSoftmaxVariantDataloaderTest.py \
+    --task_name 4CWS_CWS \
+    --model_type sequencelabeling \
+    --data_dir ../data/CWS/BMES/MSR/ \
+    --bert_model_dir ../models/bert-base-chinese/ \
+    --vocab_file ../models/bert-base-chinese/vocab.txt \
+    --do_lower_case True \
+    --max_seq_length 128 \
+    --init_checkpoint ../models/bert-base-chinese\
+    --override_output True \
+    --learning_rate 2e-5 \
+    --method fine_tune \
+    --output_dir ./tmp/4CWS/MSR/Softmax \
+    --num_hidden_layers 12 \
+    --train_batch_size 32 \
+    --visible_device 0 \
+    --num_train_epochs 10
---
+++ run_BertSoftmaxVariant_L11.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+python BertSoftmaxVariantDataloaderTest.py \
+    --task_name 4CWS_CWS \
+    --model_type sequencelabeling \
+    --data_dir ../data/CWS/BMES/MSR/ \
+    --bert_model_dir ../models/bert-base-chinese/ \
+    --vocab_file ../models/bert-base-chinese/vocab.txt \
+    --do_lower_case True \
+    --max_seq_length 128 \
+    --init_checkpoint ../models/bert-base-chinese\
+    --override_output True \
+    --learning_rate 1e-4 \
+    --method last_layer \
+    --output_dir ./tmp/4CWS/MSR/Softmax_BiLSTM_l11 \
+    --num_hidden_layers 11 \
+    --train_batch_size 128 \
+    --visible_device 3 \
+    --num_train_epochs 10
