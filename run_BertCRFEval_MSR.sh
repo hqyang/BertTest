80d3b71d9da5e713041cd8879505b194324848b2
Haiqin Yang <haiqin.yang@gmail.com>
2019-05-13 00:12:40 +0800

    update eval scripts

--- run_BertCRFEval_MSR.sh
+++ run_BertCRFEval_MSR.sh
@@ -17,7 +17,7 @@ python eval.py \
     --method fine_tune \
     --num_hidden_layers 12 \
     --train_batch_size 32 \
-    --visible_device 3 \
+    --visible_device 0 \
 
 # --task_name PKU \ #  MSR \
 # --data_dir ../data/CWS/BMES/ # need add MSR/
