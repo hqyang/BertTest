d2fda3b3643872b90bb17d7e197645bfae16ad8c
Haiqin Yang <haiqin.yang@gmail.com>
2019-05-13 19:35:49 +0800

    restore computing crossentropy in customize_modeling.py

--- src/customize_modeling.py
+++ src/customize_modeling.py
@@ -1143,9 +1143,6 @@ class BertVariant(PreTrainedBertModel):
         # mask is a ByteTensor
 
         if self.fclassifier == 'Softmax':
-            logits = torch.masked_select(logits, mask)
-            labels = torch.masked_select(labels, mask)
-
             loss_fct = nn.CrossEntropyLoss()
             loss = loss_fct(logits.view(-1, self.num_tags), labels.view(-1))
         elif self.fclassifier == 'CRF':
@@ -1283,9 +1280,6 @@ class BertCWS(PreTrainedBertModel):
         # mask is a ByteTensor
 
         if self.fclassifier == 'Softmax':
-            logits = torch.masked_select(logits, mask)
-            labels = torch.masked_select(labels, mask)
-
             loss_fct = nn.CrossEntropyLoss()
             loss = loss_fct(logits.view(-1, self.num_tags), labels.view(-1), mask.view(-1))
         elif self.fclassifier == 'CRF':
