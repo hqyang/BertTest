e1d0c30030ae47586b63213aa411bc53e3ff07f4
Haiqin Yang <haiqin.yang@gmail.com>
2019-05-30 12:28:47 +0800

    add src/__init__.py to fix relative import error

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+#!/anaconda3/envs/haiqin370/bin/ python3
+# -*- coding: utf-8 -*-
+"""
+Created on at 12:27 2019-05-30 
+@author: haiqinyang
+
+Feature: 
+
+Scenario: 
+"""
--- src/customize_modeling.py
+++ src/customize_modeling.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('../src')
 import torch
 import torch.nn as nn
 import math
