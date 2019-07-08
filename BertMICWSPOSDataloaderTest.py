8f04663684e69ee0234d8742e9eb47e020ea51e2
Haiqin Yang <haiqin.yang@gmail.com>
2019-07-08 15:59:25 +0800

    update BertMICWSPOSDataloaderTest.py

--- BertMICWSPOSDataloaderTest.py
+++ BertMICWSPOSDataloaderTest.py
@@ -599,7 +599,7 @@ def set_local_Ontonotes_param():
 def set_server_Ontonotes_param():
     return {'task_name': 'ontonotes_cws_pos2.0',
             'model_type': 'sequencelabeling',
-            'data_dir': '../data/ontonotes5/4nerpos_update/valid/',
+            'data_dir': '../data/ontonotes5/4nerpos_update/',
             'vocab_file': './src/BERT/models/multi_cased_L-12_H-768_A-12/vocab.txt',
             'bert_config_file': './src/BERT/models/multi_cased_L-12_H-768_A-12/bert_config.json',
             'output_dir': './tmp/ontonotes/CWSPOS2/',
@@ -619,9 +619,9 @@ def set_server_Ontonotes_param():
 
 
 TEST_FLAG = False
-TEST_FLAG = True
+#TEST_FLAG = True
 isServer = True
-isServer = False
+#isServer = False
 
 def main(**kwargs):
     if TEST_FLAG:
