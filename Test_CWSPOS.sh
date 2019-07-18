8b5d69ed94b90f2eff7e390f3d7754ff624f30fd
Haiqin Yang <haiqin.yang@gmail.com>
2019-07-18 11:08:45 +0800

    update BertCWSPOSDataloaderTest.py

--- Test_CWSPOS.sh
+++ Test_CWSPOS.sh
@@ -11,16 +11,20 @@ do
         --data_dir ../data/ontonotes5/4nerpos_data \
         --output_dir ./tmp/ontonotes \
         --fclassifier Softmax \
-        --bert_model_dir ../models/bert-base-chinese/ \
-        --vocab_file ../models/bert-base-chinese/vocab.txt \
+        --bert_model_dir ../models/multi_cased_L-12_H-768_A-12/ \
+        --vocab_file ../models/multi_cased_L-12_H-768_A-12/vocab.txt \
         --do_lower_case True \
         --max_seq_length 128 \
-        --init_checkpoint ../models/bert-base-chinese \
+        --init_checkpoint ../models/multi_cased_L-12_H-768_A-12 \
         --override_output True \
         --learning_rate 2e-5 \
         --method fine_tune \
         --num_hidden_layers $1 \
         --train_batch_size $2 \
         --visible_device 0 \
-        --num_train_epochs 10
+        --num_train_epochs 20
 done
+
+#         --init_checkpoint ../models/bert-base-chinese \
+#         --vocab_file ../models/bert-base-chinese/vocab.txt \
+#         --bert_model_dir ../models/bert-base-chinese/ \
