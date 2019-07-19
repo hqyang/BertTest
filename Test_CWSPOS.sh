b2d406b36a1d74a06d1b5dad39c2e706693b3e80
Haiqin Yang <haiqin.yang@gmail.com>
2019-07-19 12:34:06 +0800

    update setting in Test_CWSPOS.sh

--- Test_CWSPOS.sh
+++ Test_CWSPOS.sh
@@ -13,7 +13,7 @@ do
         --fclassifier Softmax \
         --bert_model_dir ../models/multi_cased_L-12_H-768_A-12/ \
         --vocab_file ../models/multi_cased_L-12_H-768_A-12/vocab.txt \
-        --do_lower_case True \
+        --do_lower_case False \
         --max_seq_length 128 \
         --init_checkpoint ../models/multi_cased_L-12_H-768_A-12 \
         --override_output True \
