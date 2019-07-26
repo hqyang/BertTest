21b420df7eec6c36e766ae2d45227ecd4eb2f937
Haiqin Yang <haiqin.yang@gmail.com>
2019-07-26 11:54:55 +0800

    do_lower_case=False in Test_CWSPOS2.sh

--- Test_CWSPOS2.sh
+++ Test_CWSPOS2.sh
@@ -9,11 +9,11 @@ do
         --task_name ontonotes_cws_pos2.0 \
         --model_type sequencelabeling \
         --data_dir ../data/ontonotes5/4nerpos_update \
-        --output_dir ./tmp/ontonotes/CWSPOS2/ \
+        --output_dir ./tmp/ontonotes/CWSPOS2/case/ \
         --fclassifier Softmax \
         --bert_model_dir ../models/multi_cased_L-12_H-768_A-12/ \
         --vocab_file ./src/BERT/models/multi_cased_L-12_H-768_A-12/vocab.txt \
-        --do_lower_case True \
+        --do_lower_case False \
         --max_seq_length 128 \
         --init_checkpoint ../models/multi_cased_L-12_H-768_A-12/ \
         --override_output True \
